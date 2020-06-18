d7866e4c926220d40615f92567d48834ff3d4edd
Shantanu Ghosh <beingshantanu2406@gmail.com>
2020-06-18 02:08:07 -0400

    remove comment

--- main_propensity_dropout.py
+++ main_propensity_dropout.py
@@ -2,6 +2,6 @@ from Graphs import Graphs
 from DCN_PD_deep import DCN_PD_Deep
 
 if __name__ == '__main__':
-    # DCN_PD_Deep().execute_DCN_PD()
+    DCN_PD_Deep().execute_DCN_PD()
     Graphs().draw_scatter_plots()
 
